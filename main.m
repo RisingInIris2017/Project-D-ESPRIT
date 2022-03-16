a5438fab9516af6233aada79104d878b236bdb81
Isaac Thompson <risinginiris2017@gmail.com>
2022-03-16 23:25:47 +0800

    20220316

--- main.m
+++ main.m
@@ -1,8 +1,10 @@
+%% 信号的参数
 % 设置独立信源的个数
 L = 2;
 % 设置子阵的个数
 number_of_subarray = 3;
-% 设置子阵中阵元的个数
+% 设置子阵中阵元的个数，这个参数在文献[1]中就是Mk
+% 我们架设所有的子阵阵元个数都相同
 antenna_in_subarray = 5;
 % 总阵元个数
 M = number_of_subarray * antenna_in_subarray;
@@ -12,5 +14,13 @@ doa_vector = 10*(1:L);
 Ps = 0;
 SNR = 20;
 Pn = Ps - SNR;
+% 设置信号时间采样的快拍数
+N = 1000;
+
+%% 幂方法的参数
+% 设置幂方法的迭代次数Q
+Q = 5;
+
+%% 生成物理量
 % 生成各独立信号的时域采样构成的向量
-signal_vector = signal_generator(L);
+signal_vector = signal_generator(Ps, L, N);
--- signal_generator.m
+++ signal_generator.m
@@ -1,13 +1,10 @@
-function signal_vector = signal_generator(L)
+function signal_vector = signal_generator(Ps, L, N)
 % 构造单音信号的时间序列
 % 设置单频正弦信号的频率
 omega_vector = 1:L;
     % 设置时间序列的采样点数
-    N = 1000;
     t = 0:N-1;
     signal_vector = ones(L,N);
-    % 设信号功率为0dB，用于规定信号幅值
-    Ps = 0;
 for index = 1:L
     signal_vector(index,:)=10^(Ps/10)*sin(2*pi*omega_vector(index)/(N-1)*t);
 end
