55cd7046b37d23cc8718b565c1df6cb5d04a85c8
Isaac Thompson <risinginiris2017@gmail.com>
2022-03-27 23:26:23 +0800

    纠正Es生成算法中第二维下标固定为L的bug，但出现了B矩阵总不可逆的问题有待解决

--- new_dpm.m
+++ new_dpm.m
@@ -12,10 +12,10 @@ antenna_in_subarray = 4;
 % 总阵元个数
 M = number_of_subarray * antenna_in_subarray;
 % 设置真实 DOA
-doa_vector = [10 30];
+doa_vector = [30 30];
 % 设置信号功率和信噪比，均为分贝，并求得噪声功率
 Ps = 0;
-SNR = 20;
+SNR = 10;
 Pn = Ps - SNR;
 % 设置信号时间采样的快拍数
 N = 1000;
@@ -40,6 +40,7 @@ for k = 1:number_of_subarray
 end
 xkg_command = extractBefore(xkg_command,strlength(xkg_command))+"] = xk_generator(Ps, L, N, SNR, number_of_subarray, doa_vector, antenna_in_subarray);";
 eval(xkg_command);
+clear xkg_command;
 
 %% AC迭代的参数
 % 在这个脚本变为函数后，这一段应删去
@@ -51,8 +52,7 @@ laplacianMatrix = [1,-1,0;0,2,-1;0,0,1];
 % 拉普拉斯矩阵前面的标量系数alpha必须大于0
 % 最佳的alpha取值为最大的特征值与第二小的特征值算术平均取倒数
 eigLapMat = sort(eig(laplacianMatrix),'descend');
-% alpha = 2/(eigLapMat(1) + eigLapMat(number_of_subarray - 1));
-alpha = 0.5;
+alpha = 2/(eigLapMat(1) + eigLapMat(number_of_subarray - 1));
 W = eye(number_of_subarray) - alpha * laplacianMatrix;
 
 
@@ -61,8 +61,8 @@ W = eye(number_of_subarray) - alpha * laplacianMatrix;
 PM = 10;
 % 生成迭代初始值
 % 迭代初始的Es（Us，信号子空间）矩阵，每antenna_in_subarray行归属一个子阵，每列归属一个信号
-% Es = rand(antenna_in_subarray * number_of_subarray, L);
-Es = 0.8*ones(antenna_in_subarray * number_of_subarray, L);
+Es = rand(antenna_in_subarray * number_of_subarray, L);
+% Es = 0.6*ones(antenna_in_subarray * number_of_subarray, L);
 % 迭代初始的a矩阵，每列归属一个t时刻，每行均分给各个子阵，每层归属一个信号
 a = zeros(N, number_of_subarray, L);
 for pm = 1:PM
@@ -77,26 +77,45 @@ for pm = 1:PM
     end
     % 暂时用求算术平均代替AC
     at = number_of_subarray * mean(a,2);
+    at = squeeze(at);
     for q = 1:L
         for p = 1:number_of_subarray
             for t = 1:N
-                Es((p-1)*antenna_in_subarray+1:p*antenna_in_subarray,L) = Es((p-1)*antenna_in_subarray+1:p*antenna_in_subarray,L) + eval("x_"+num2str(p)+"(:,t)*at(t,1,L);");
+                Es((p-1)*antenna_in_subarray+1:p*antenna_in_subarray,q) = ...
+                    Es((p-1)*antenna_in_subarray+1:p*antenna_in_subarray,q) + eval("x_"+num2str(p)+"(:,t)")*at(t,q);
             end
-            Es((p-1)*antenna_in_subarray+1:p*antenna_in_subarray,L) = Es((p-1)*antenna_in_subarray+1:p*antenna_in_subarray,L)/N;
+            Es((p-1)*antenna_in_subarray+1:p*antenna_in_subarray,q) = Es((p-1)*antenna_in_subarray+1:p*antenna_in_subarray,q)/N;
         end  
     end
+    
 end
 % 暂时用简单的方法归一化，之后需用AC替代
 for q = 1:L
     for p = 1:number_of_subarray
-        Es((p-1)*antenna_in_subarray+1:p*antenna_in_subarray,L) = Es((p-1)*antenna_in_subarray+1:p*antenna_in_subarray,L)/vecnorm(Es((p-1)*antenna_in_subarray+1:p*antenna_in_subarray,L));
+        Es((p-1)*antenna_in_subarray+1:p*antenna_in_subarray,q) = Es((p-1)*antenna_in_subarray+1:p*antenna_in_subarray,q)/vecnorm(Es((p-1)*antenna_in_subarray+1:p*antenna_in_subarray,q));
     end  
 end
-% 暂时用J算子直接处理Es，检验Es的生成算法是否有效
+% 用J算子分布式处理Es，暂时用直接求和代替AC
 % 之后需另外重写算法替代
-Es_upper = Jupper*Es;
-Es_lower = Jlower*Es;
-eig_psi = eig((Es_upper'*Es_upper)\Es_upper'*Es_lower);
-phase_psi=atan2(imag(eig_psi),real(eig_psi));
-doa_estimate = sort(abs(asin(phase_psi/pi)'*180/pi));
-error = abs(doa_estimate - doa_vector)
+% 矩阵B与矩阵W都来自文献1-6
+Bmat = zeros(L,L);
+Wmat = zeros(L,L);
+for index1 = 1:L
+    e_i = Es(:,index1);
+    e_i_k = reshape(e_i,antenna_in_subarray,number_of_subarray);
+    for index2 = 1:L
+        e_j = Es(:,index2);
+        e_j_k = reshape(e_j,antenna_in_subarray,number_of_subarray);
+        for p = 1:number_of_subarray
+            Bmat(index1,index2) = Bmat(index1,index2) + (Jupperk*e_i_k(:,p))'*(Jupperk*e_j_k(:,p));
+            Wmat(index1,index2) = Wmat(index1,index2) + (Jupperk*e_i_k(:,p))'*(Jlowerk*e_j_k(:,p));
+        end
+    end
+end
+
+det(Bmat)
+% psi = Bmat\Wmat;
+
+% phase_psi=atan2(imag(eig_psi),real(eig_psi));
+% doa_estimate = sort(abs(asin(phase_psi/pi)'*180/pi));
+% error = abs(doa_estimate - doa_vector)
